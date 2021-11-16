a3d450f9c1272d31b63bbd4c87c32ce4c4f246c7
Dominic Cabitac <dominic.cabitac@uoit.net>
2021-11-16 12:23:17 -0500

    Hero done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeroComponent } from './hero/hero.component';
+import { BackgroundComponent } from './hero/background/background.component';
+import { TypeWritterComponent } from './hero/type-writter/type-writter.component';
+import { CtaComponent } from './hero/cta/cta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    HeroComponent
+    HeroComponent,
+    BackgroundComponent,
+    TypeWritterComponent,
+    CtaComponent
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
   imports: [
     BrowserModule,
     AppRoutingModule
