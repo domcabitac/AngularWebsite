6ccd607f3f39d71251e12f3cd88dee5e843c40ad
Dominic Cabitac <dominic.cabitac@uoit.net>
2021-11-16 12:05:39 -0500

    Added hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
