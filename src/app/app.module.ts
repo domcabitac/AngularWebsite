b41f3b94776c5dfec5976cd6408f110dbbc8d581
Dominic Cabitac <dominic.cabitac@uoit.net>
2022-04-14 13:42:33 -0400

    Removed CTA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { HeroComponent } from './hero/hero.component';
 import { BackgroundComponent } from './hero/background/background.component';
 import { TypeWritterComponent } from './hero/type-writter/type-writter.component';
-import { CtaComponent } from './hero/cta/cta.component';
 import { ServiceComponent } from './service/service.component';
 import { CardsComponent } from './service/cards/cards.component';
 import { AboutComponent } from './about/about.component';
@@ -25,7 +24,6 @@ import { CareerComponent } from './career/career.component';
     HeroComponent,
     BackgroundComponent,
     TypeWritterComponent,
-    CtaComponent,
     ServiceComponent,
     CardsComponent,
     AboutComponent,
