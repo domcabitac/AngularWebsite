15ce4f6ea0560f7f750913c6426f120f3d42de03
Dominic Cabitac <dominic.cabitac@uoit.net>
2024-01-13 20:35:59 -0500

    Updated brands to sliders

--- src/app/code/brands/brands.component.ts
+++ src/app/code/brands/brands.component.ts
@@ -16,20 +16,32 @@ export class BrandsComponent implements OnInit {
     logo : '/assets/img/Logos/FC.svg',
     link : 'https://www.ferrazcreative.com/'
   },{
+    logo : '/assets/img/Logos/OPG.svg',
+    link : 'https://www.opg.com/'
+  },{
+    logo : '/assets/img/Logos/Ver.svg',
+    link : 'https://verifytab.com/'
+  },{
+
+    logo : '/assets/img/Logos/SPA.svg',
+    link : 'https://wearesparkent.com/'
+  },{
     logo : '/assets/img/Logos/BP.svg',
     link : 'https://www.brucepower.com/'
   },{
-    logo : '/assets/img/Logos/NPX.svg',
-    link : 'https://www.npxinnovation.ca'
+    logo : '/assets/img/Logos/UWDR.svg',
+    link : 'https://unitedwaydr.com/'
   },{
     logo : '/assets/img/Logos/RBC.svg',
     link : 'https://www.rbcroyalbank.com/personal.html'
   },{
-    logo : '/assets/img/Logos/OPG.svg',
-    link : 'https://www.opg.com/'
+    logo : '/assets/img/Logos/PG.svg',
+    link : 'https://parablegifts.ca/'
   },{
-    logo : '/assets/img/Logos/UWDR.svg',
-    link : 'https://unitedwaydr.com/'
-  }]
+
+    logo : '/assets/img/Logos/NPX.svg',
+    link : 'https://www.npxinnovation.ca'
+  }
+]
 
 }
